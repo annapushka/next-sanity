ec2a76db38a927d8c4fc518a994434613d6ef9f9
Anna Pushkareva <smirnova_spb@mail.ru>
2023-05-01 16:25:32 +0300

    add NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const projects = await getProjects()
 
   return (
-    <div className="max-w-5xl mx-auto py-20 ">
+    <div className="">
       <h1 className="text-7xl font-extrabold">Hello I'm <span className="bg-gradient-to-r from-pink-500 via-blue-300 to-violet-500 bg-clip-text text-transparent">Anna</span>!</h1>
       <p className="mt-3 text-xl text-gray-600">I'm a Frontend Developer based in SPb.</p>
       <h2 className="mt-24 text-3xl font-bold text-gray-700">My Projects</h2>
