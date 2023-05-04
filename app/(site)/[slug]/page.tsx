284cd8c3d5156d26a3a60f12aaef343e5e62de9d
Anna Pushkareva <smirnova_spb@mail.ru>
2023-05-04 21:25:52 +0300

    add pages schema

---
+++ app/(site)/[slug]/page.tsx
@@ -0,0 +1,21 @@
+
+'use client';
+import { getPage } from "@/sanity/sanity-utils"
+import { PortableText } from "@portabletext/react";
+
+type Props = {
+    params: { slug: string }
+}
+
+export default async function Page({ params }: Props) {
+    const page = await getPage(params.slug);
+
+    return (
+        <div>
+            <h1 className="bg-gradient-to-r from-pink-500 via-blue-300 to-violet-500 bg-clip-text text-transparent text-5xl drop-shadow-lg font-extrabold">{page.title}</h1>
+            <div className="text-lg text-gray-700 mt-10">
+                <PortableText value={page.content} />
+            </div>
+        </div>
+    )
+}
